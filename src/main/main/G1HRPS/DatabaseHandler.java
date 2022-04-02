d939a771c8fc15fa1ad66dd54561b706cd128355
Steph Wong <12522345+stephwongwt@users.noreply.github.com>
2022-04-02 21:36:09 +0800

    Implement database handler with read/write functions

---
+++ src/main/main/G1HRPS/DatabaseHandler.java
@@ -0,0 +1,41 @@
+package main.G1HRPS;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class DatabaseHandler {
+    public static final String SEPARATOR = "|";
+
+    /** Write fixed content to the given file. */
+    public static void write(String fileName, List data) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(fileName));
+
+        try {
+            for (int i = 0; i < data.size(); i++) {
+                out.println((String) data.get(i));
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    /** Read the contents of the given file. */
+    public static List read(String fileName) throws IOException {
+        List data = new ArrayList();
+        Scanner scanner = new Scanner(new FileInputStream(fileName));
+        try {
+            while (scanner.hasNextLine()) {
+                data.add(scanner.nextLine());
+            }
+        } finally {
+            scanner.close();
+        }
+        return data;
+    }
+}
