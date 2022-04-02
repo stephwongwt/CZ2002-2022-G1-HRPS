ad5951ff8bbe1e3cdb1c8e9f1a193696c8fc1c38
notingcode <notingcode@gmail.com>
2022-04-03 00:42:50 +0800

    quick implementation

--- src/main/payment/main/G1HRPS/PaymentManager.java
+++ src/main/payment/main/G1HRPS/PaymentManager.java
@@ -1,13 +1,26 @@
-package main.G1HRPS;
+package main.payment.main.G1HRPS;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import main.main.G1HRPS.CodeGen;
+import main.main.G1HRPS.Supermanager;
 
 public class PaymentManager implements Supermanager<Payment>, CodeGen {
 
 	private List<Payment> payment_list_;
+	public static final String SEPARATOR = "|";
 
 	public PaymentManager() {
 		// TODO - implement PaymentManager.PaymentManager
+
+		InitializeDB();
+
 		throw new UnsupportedOperationException();
 	}
 
@@ -16,6 +29,9 @@ public class PaymentManager implements Supermanager<Payment>, CodeGen {
 	 */
 	public void AddToList(Payment payment) {
 		// TODO - implement PaymentManager.AddToList
+
+		payment_list_.add(payment);
+
 		throw new UnsupportedOperationException();
 	}
 
@@ -24,37 +40,65 @@ public class PaymentManager implements Supermanager<Payment>, CodeGen {
 	 */
 	public void RemoveFromList(Payment payment) {
 		// TODO - implement PaymentManager.RemoveFromList
+
+		payment_list_.remove(payment);
+
 		throw new UnsupportedOperationException();
 	}
 
-	public void SearchList(String search_text) {
+	@Override
+	public Payment SearchList(String payment_id) {
 		// TODO - implement PaymentManager.SearchList
-		throw new UnsupportedOperationException();
+
+		for(Payment payment : payment_list_){
+			if(payment_id.equals(payment.GetPaymentID())){
+				return payment;
+			}
+		}
+
+		return null;
+
+		// throw new UnsupportedOperationException();
 	}
 
 	public List<Payment> GetList() {
 		// TODO - implement PaymentManager.GetList
-		throw new UnsupportedOperationException();
+
+		return payment_list_;
+
+		// throw new UnsupportedOperationException();
 	}
 
 	public void InitializeDB() {
 		// TODO - implement PaymentManager.InitializeDB
+
+		
+
 		throw new UnsupportedOperationException();
 	}
 
 	public void SaveDB() {
 		// TODO - implement PaymentManager.SaveDB
+
+
+
 		throw new UnsupportedOperationException();
 	}
 
-	public void SetPaymentStatus() {
+	public void SetPaymentStatus(Payment payment, PaymentStatus status) {
 		// TODO - implement PaymentManager.SetPaymentStatus
+
+		payment.SetStatus(status);
+
 		throw new UnsupportedOperationException();
 	}
 
-	public void GetPaymentStatus() {
+	public PaymentStatus GetPaymentStatus(Payment payment) {
 		// TODO - implement PaymentManager.GetPaymentStatus
-		throw new UnsupportedOperationException();
+
+		return payment.GetStatus();
+
+		// throw new UnsupportedOperationException();
 	}
 
 	/**
@@ -62,12 +106,99 @@ public class PaymentManager implements Supermanager<Payment>, CodeGen {
 	 */
 	public void GenerateAndPrintBill() {
 		// TODO - implement PaymentManager.GenerateAndPrintBill
+
+		
+
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public String GenerateCode(String prefix) {
 		// TODO Auto-generated method stub
-		return null;
+		long stamp;
+
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+		stamp = timestamp.getTime();
+
+		return prefix + String.valueOf(stamp);
+	}
+
+	public static List<Payment> readGuests(String filename) throws IOException {
+		List<String> stringArray = read(filename);
+		List<Payment> guestList = new ArrayList<>();
+
+		for(String st : stringArray){
+			StringTokenizer star = new StringTokenizer(st, SEPARATOR);
+			
+			String payment_id = star.nextToken().trim();
+			int guest_id = Integer.parseInt(star.nextToken().trim());
+			float discounts = Float.parseFloat(star.nextToken().trim());
+			float tax = Float.parseFloat(star.nextToken().trim());
+			float bill_total = Float.parseFloat(star.nextToken().trim());
+			PaymentStatus status = PaymentStatus.valueOf(star.nextToken().trim());
+			
+			Payment payment = new Payment(discounts, bill_total, payment_id);
+
+			payment.SetTax(tax);
+			payment.SetGuestId(guest_id);
+			payment.SetStatus(status);
+
+			guestList.add(payment);
+		}
+
+		return guestList;
+	}
+
+	public static void savePayments(String fileName, List<Payment> paymentList) throws IOException {
+		List<String> paymentData = new ArrayList<>();
+
+		for(Payment payment : paymentList){
+			StringBuilder st = new StringBuilder();
+
+			st.append(payment.GetPaymentID());
+			st.append(SEPARATOR);
+			st.append(payment.GetGuestId());
+			st.append(SEPARATOR);
+			st.append(payment.GetDiscount());
+			st.append(SEPARATOR);
+			st.append(payment.GetTax());
+			st.append(SEPARATOR);
+			st.append(payment.GetTotalBill());
+			st.append(SEPARATOR);
+			st.append(payment.GetStatus().toString());
+			st.append(SEPARATOR);
+
+			paymentData.add(st.toString());
+		}
+		write(fileName, paymentData);
+	}
+
+	public static List<String> read(String fileName) throws IOException {
+		List<String> data = new ArrayList<>();
+		Scanner sc = new Scanner(new FileInputStream(fileName));
+		try{
+			while(sc.hasNextLine()){
+				data.add(sc.nextLine());
+			}
+		}		
+		finally{
+			sc.close();
+		}
+
+		return data;
+	}
+
+	public static void write(String fileName, List<String> data) throws IOException {
+		PrintWriter out = new PrintWriter(fileName);
+
+		try{
+			for(int i = 0; i < data.size(); i++){
+				out.println(data.get(i));
+			}
+		}
+		finally{
+			out.close();
+		}
 	}
 }
