b9295902b7cb4ab7c56db72dd685d90ac32f2cf8
notingcode <notingcode@gmail.com>
2022-04-05 16:36:24 +0800

    revert changes to menu items and room service

--- src/main/menuitem/main/G1HRPS/MenuItem.java
+++ src/main/menuitem/main/G1HRPS/MenuItem.java
@@ -14,26 +14,7 @@ public class MenuItem {
 	 */
 	public MenuItem(String name, float price, String description) {
 		// TODO - implement MenuItem.MenuItem
-		this.name_ = name;
-		this.price_ = price;
-		this.description_ = description;
+		throw new UnsupportedOperationException();
 	}
-	public String toString()
-	{
-		return "Item: "+ this.name_ + " Price: $" + this.price_ + " Description: " + this.description_;
-	}
-	public String getName() {
-		return name_;
-	}
-	public void setName(String name) {
-		this.name_ = name;
-	}
-	public float getPrice() {
-		return price_;
-	}
-	public void setPrice(float price) {
-		this.price_ = price;
-	}
-	
 
 }
--- src/main/menuitem/main/G1HRPS/MenuItemManager.java
+++ src/main/menuitem/main/G1HRPS/MenuItemManager.java
@@ -1,7 +1,7 @@
 package main.G1HRPS;
 
 import java.util.List;
-import java.util.ArrayList;
+
 /**
  * Manages the available menu items for room service
  */
@@ -10,27 +10,14 @@ public class MenuItemManager implements Supermanager<MenuItem> {
 	public List<MenuItem> menu_item_list_;
 
 	public MenuItemManager() {
-		// TODO - implement MenuItemManager.MenuItemManager
-		this.menu_item_list_ = new ArrayList<MenuItem>();
-		InitializeDB();
 	}
 
 	/**
 	 * Takes in an class object and list to add the object into.
 	 */
-	// User is asked for 3 inputs on Meny before this method is called
 	public void AddToList(MenuItem menu_item) {
 		// TODO - implement MenuItemManager.AddToList
-		for (int i = 0; i < this.menu_item_list_.size(); i++)
-		{
-			if (this.menu_item_list_.get(i).getName() == menu_item.getName())
-			{
-				System.out.println("Item with the same name already exists. This will not to be added");
-				return;
-			}
-		}
-		this.menu_item_list_.add(menu_item);
-		System.out.println("Item " + menu_item.getName() + " added");
+		throw new UnsupportedOperationException();
 	}
 
 	/**
@@ -38,29 +25,12 @@ public class MenuItemManager implements Supermanager<MenuItem> {
 	 */
 	public void RemoveFromList(MenuItem menu_item) {
 		// TODO - implement MenuItemManager.RemoveFromList
-		for (int i = 0; i < this.menu_item_list_.size(); i++)
-		{
-			if (this.menu_item_list_.get(i).getName() == menu_item.getName())
-			{
-				this.menu_item_list_.remove(i);
-				System.out.println("Item " + menu_item.getName() + " removed");
-				return;
-			}
-		}
-		System.out.println("Item to be removed not found in Menu!");
-		
+		throw new UnsupportedOperationException();
 	}
-	// What is this for exactly ?????
+
 	public void SearchList(String search_text) {
 		// TODO - implement MenuItemManager.SearchList
-		for (int i = 0; i < this.menu_item_list_.size(); i++)
-		{
-			if (this.menu_item_list_.get(i).getName() == search_text)
-			{
-				System.out.println(this.menu_item_list_.get(i));
-				return;
-			}
-		}
+		throw new UnsupportedOperationException();
 	}
 
 	/**
@@ -68,13 +38,7 @@ public class MenuItemManager implements Supermanager<MenuItem> {
 	 */
 	public List<MenuItem> GetList() {
 		// TODO - implement MenuItemManager.GetList
-		System.out.println("========= Menu List =========");
-		for (int i = 0; i < this.menu_item_list_.size(); i++)
-		{
-			System.out.println(this.menu_item_list_.get(i));
-		}
-		System.out.println("========= End of Menu =========");
-		return this.menu_item_list_;
+		throw new UnsupportedOperationException();
 	}
 
 	public void InitializeDB() {
@@ -92,17 +56,7 @@ public class MenuItemManager implements Supermanager<MenuItem> {
 	 */
 	public void EditMenuItemPrice(String item_name, float new_price) {
 		// TODO - implement MenuItemManager.EditMenuItemPrice
-		for(int i = 0; i < this.menu_item_list_.size(); i++)
-		{
-			if (this.menu_item_list_.get(i).getName() == item_name )
-			{
-				this.menu_item_list_.get(i).setPrice(new_price);
-				System.out.println("Price for " + item_name + " amended!");
-				return;
-			}
-		}
-		System.out.println("Item not found in Menu!");
-		
+		throw new UnsupportedOperationException();
 	}
 
 }
