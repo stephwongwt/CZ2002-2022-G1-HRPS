4e00d708c5fc4a6f6c2f639a91880979086fa4f4
Steph Wong <12522345+stephwongwt@users.noreply.github.com>
2022-04-15 12:44:35 +0800

    clean up AppManager - not completed yet.

--- src/main/main/G1HRPS/AppManager.java
+++ src/main/main/G1HRPS/AppManager.java
@@ -1,12 +1,19 @@
 package main.G1HRPS;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Vector;
 
 /**
  * Manager that handles running the app
  */
 public class AppManager {
+    public static final DateTimeFormatter datetime_formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public Scanner sc_;
     private ReservationManager reservation_manager_;
     private GuestManager guest_manager_;
@@ -39,6 +46,8 @@ public class AppManager {
         room_service_manager_.InitializeDB();
         room_manager_.InitializeDB();
         menu_item_manager_.InitializeDB();
+
+        ///TODO: rooms need to be populated with guests again on init
     }
 
     /**
@@ -62,36 +71,96 @@ public class AppManager {
                     CreateNewRoom();
                     break;
                 case SearchGuest:
-                    Guest search_guest = SearchList(guest_manager_);
-                    // search_guest.toString();
+                    Guest guest = SearchList(guest_manager_);
+                    guest.toString();
+                    final String guest_id = guest.GetIdentity();
+                    final int guest_room_number = guest.GetRoomNum();
 
-                    /// check in guest & room & reservation
-                    // check if guest has reservation
-                    // reservation_manager_.SearchListByGuestId(search_guest.GetIdentity());
-                    // reservation exists, check in
-                    // guest_manager_.CheckInGuest(search_guest, room_num);
+                    System.out.println("Which to display?\n" +
+                            "[0] Go back\n" +
+                            "[1] Check In\n" +
+                            "[2] Check Out\n" +
+                            "[3] Order Room Service\n" +
+                            "[4] Edit Details");
+                    System.out.println("What would you like to do with this Guest?");
+                    int opt = 0;
+                    while (true) {
+                        try {
+                            opt = sc_.nextInt();
+                            if (opt != 0) {
+                                sc_.nextLine();
+                                break;
+                            }
+                        } catch (Exception e) {
+                            sc_.nextLine();
+                            System.out.println("Unavailable, please try again.");
+                        }
+                    }
 
-                    // guest has no reservation
-                    // room_manager_.GetRoomStatisticsByTypeOccupancyRate();
-                    // ask for input to pick room
-                    // guest_manager_.CheckInGuest(search_guest, room_num);
+                    switch (opt) {
+                        case 0:
+                            break;
+                        case 1:
+                        /// check in guest & room & 
+                            System.out.println("|-----|Check In|-----|");
+                            Reservation check_in_reservation = reservation_manager_.SearchList(guest);
+                            int room_to_check_in = 0;
+                            if (check_in_reservation != null) {
+                                if (reservation_manager_.CheckIn(check_in_reservation)) {
+                                    room_to_check_in = check_in_reservation.GetRoomNum();
+                                    System.out.println("Guest reservation found!");
+                                }
+                            } else {
+                                room_to_check_in = PickRoom();
+                            }
+                            System.out.println("Room Number: " + room_to_check_in);
+                            guest_manager_.CheckIntoRoom(guest, room_to_check_in);
+                            room_manager_.CheckInGuest(guest, room_to_check_in);
+                            System.out.println("Guest checked in.");
+                            break;
+                        case 2:
+                            System.out.println("|-----|Check Out|-----|");
+                            System.out.println("Enter discount percentage (e.g. 10 -> 10%): ");
+                            int discounts = GetIntFromInput();
+                            System.out.println("Enter tax percentage for Guest (e.g. 10 -> 10%): ");
+                            int tax = GetIntFromInput();
 
+                            Room guest_room = room_manager_.SearchList(guest);
+                            List<RoomServiceOrder> room_service_orders = room_service_manager_.GetOrderedItemsByRoom(guest_room_number);
+                            Payment bill = payment_manager_.GenerateAndPrintBill(guest_id, guest_room_number, LocalDateTime.parse(guest.GetCheckInDate(), datetime_formatter), guest_room.GetRoomPrice(), discounts, tax, room_service_orders);
+                            bill.toString();
 
-                    /// check out guest & room & reservation
-                    // Payment checkout = payment_manager_.GenerateAndPrintBill();
-                    // ask if making payment
-                    // yes
-                    // guest_manager_.CheckOutGuest(search_guest, checkout.GetPaymentId());
-                    // room_manager_.CheckOutAllGuest(search_guest.GetRoomNum());
-                    // no
-                    // exit submenu
+                            System.out.println("Making payment for this bill? (False[0]/True[1])");
+                            boolean make_payment = GetBooleanFromInput();
+                            if (make_payment) {
+                                guest_manager_.CheckOutOfRoom(guest, bill.GetPaymentID());
+                                room_manager_.CheckOutGuests(bill.GetRoomNum());
+                                Reservation check_out_reservation = reservation_manager_.SearchList(guest);
+                                reservation_manager_.CheckOut(check_out_reservation);
+                            }
+                            break;
+                        case 3:
+                            System.out.println("|-----|Order Room Service|-----|");
+                            List<MenuItem> ordered_item_list = PrintRoomServiceMenu();
+                            System.out.println("Any remarks to add to order?");
+                            String remarks = sc_.nextLine().toUpperCase();
+                            RoomServiceOrder room_service_order = room_service_manager_.CreateNewRoomServiceOrder(guest_id, guest_room_number, ordered_item_list, remarks);
+                            if (room_service_order != null) {
+                                System.out.println("Room Service Order successfully created!");
+                            }
+                            break;
+                        case 4:
+                            System.out.println("|-----|Edit Guest Details|-----|");
+                            boolean continue_editing = true;
+                            while (continue_editing) {
+                                System.out.println("[0] ID | [1] PaymentID | [2] RoomNum | [3] Name | [4] Credit Card No. | [5] Billing Address");
+                                System.out.println("[0] ID | [1] PaymentID | [2] RoomNum | [3] Name | [4] Credit Card No. | [5] Billing Address");
 
-                    /// edit guest menu
-
-                    /// Add room service order
-                    // boolean success = room_service_manager_.CreateNewRoomServiceOrder();
-                    // check success
-
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                     break;
                 case SearchRoom:
                     Room search_room = SearchList(room_manager_);
@@ -169,6 +238,42 @@ public class AppManager {
         return AppMenuItem.values()[option];
     }
 
+    private List<MenuItem> PrintRoomServiceMenu() {
+        List<MenuItem> picked_items_list = new ArrayList<>();
+        List<MenuItem> menu_list = menu_item_manager_.GetList();
+        int menu_list_size = menu_list.size();
+
+        System.out.println("\r\n|--------|Choose an option|--------|");
+        for (int i = 0; i < menu_list_size; i++) {
+            MenuItem menu_item = menu_list.get(i);
+            System.out.printf("[%d] %s\r\n", i, menu_item.toString());
+        }
+        System.out.printf("[%d] Complete Order\r\n", menu_list_size);
+
+        int option = -1;
+        while (option != menu_list_size) {
+            while (true) {
+                try {
+                    option = sc_.nextInt();
+                    if ((option >= 0) && (option <= menu_list_size)) {
+                        if (option != menu_list_size) {
+                            picked_items_list.add(menu_list.get(option));
+                            System.out.println(picked_items_list.toString());
+                        }
+                        break;
+                    } else {
+                        System.out.println("Unavailable, please select another option.");
+                    }
+                } catch (Exception e) {
+                    sc_.nextLine();
+                    System.out.println("Unavailable, please select another option.");
+                }
+            }
+        }
+        System.out.println(picked_items_list.toString());
+
+        return picked_items_list;
+    }
     /**
      * Handles the input and output for creating new guest
      */
@@ -194,26 +299,23 @@ public class AppManager {
         contact = sc_.nextLine().toUpperCase();
         System.out.println("Enter Country (e.g. Singapore):");
         country = sc_.nextLine().toUpperCase();
-        System.out.println("Enter Gender (e.g. Female[0]/Male[1]/Other[1]):");
+        System.out.println("Enter Gender (e.g. Female[0]/Male[1]/Other[2]):");
         gender = GetEnumFromInput(Gender.values());
         System.out.println("Enter Nationality (e.g. Singaporean):");
         nationality = sc_.nextLine().toUpperCase();
-        boolean success = guest_manager_.CreateNewGuest(identity, name, credit_card_number, address, contact, country,
-                gender, nationality);
-        if (success) {
-            System.out.printf("Guest %s successfully created", name);
+        Guest new_guest = guest_manager_.CreateNewGuest(identity, name, credit_card_number, address, contact, country, gender, nationality);
+        if (new_guest == null) {
+            System.out.println("Guest ID already exists, did not create.");
         } else {
-            System.out.println("Guest was not created. Some error happened.");
+            System.out.printf("Guest %s successfully created!", name);
         }
-        System.out.printf("%s, %s, %s, %s, %s, %s, %s, %s", identity, name, credit_card_number, address, contact,
-                country, gender.toString(), nationality);
     }
 
     /**
      * Handles the input and output for creating new room
      */
     private void CreateNewRoom() {
-        int room_num;
+        int room_number;
         RoomType room_type;
         float room_price;
         BedSize bed_size;
@@ -223,22 +325,11 @@ public class AppManager {
         RoomStatus status;
         sc_.nextLine();
         System.out.println("Enter Room Number:");
-        while (true) {
-            try {
-                room_num = sc_.nextInt();
-                if (room_num != 0) {
-                    sc_.nextLine();
-                    break;
-                }
-            } catch (Exception e) {
-                sc_.nextLine();
-                System.out.println("Unavailable, please try again.");
-            }
-        }
+        room_number = GetNonZeroIntFromInput();
         System.out.println("Enter Room Type (Single[0]/Standard[1]/VIP[2]/Suite[3]/Deluxe[4]):");
         room_type = GetEnumFromInput(RoomType.values());
         System.out.println("Enter price of room per night (e.g 100.00):");
-        room_price = sc_.nextFloat();
+        room_price = GetNonZeroFloatFromInput();
         System.out.println("Enter Bed Size (Single[0]/SuperSingle[1]/Double[2]/Queen[3]/King[4]):");
         bed_size = GetEnumFromInput(BedSize.values());
         System.out.println("Enter WiFi (False[0]/True[1]):");
@@ -249,10 +340,103 @@ public class AppManager {
         with_smoking = GetBooleanFromInput();
         System.out.println("Enter Room Status (Vacant[0]/Occupied[1]/Reserved[2]/Maintenance[3]):");
         status = GetEnumFromInput(RoomStatus.values());
-        // TODO: Add room to room manager
-        System.out.printf("%d, %s, %s, %s, %s, %s, %s, %s", room_num, room_type.toString(), room_price,
-                bed_size.toString(), Boolean.valueOf(wifi_enabled), Boolean.valueOf(with_view),
-                Boolean.valueOf(with_smoking), status.toString());
+        Room new_room = room_manager_.CreateNewRoom(room_number, room_type, room_price, bed_size, wifi_enabled, with_view, with_smoking, status);
+        if (new_room == null) {
+            System.out.println("Room ID already exists, did not create.");
+        } else {
+            System.out.printf("Room %d successfully created!", room_number);
+        }
+    }
+
+    private int PickRoom() {
+        int picked_room_number;
+        System.out.println("Pick a room number:");
+        EnumMap<RoomType, HashMap<String, Vector<Integer>>> room_stats = room_manager_.GetRoomStatisticsByTypeOccupancyRate();
+        Vector<Integer> all_available_rooms = new Vector<>();
+        for (RoomType type : RoomType.values()) {
+            HashMap<String, Vector<Integer>> stat = room_stats.get(type);
+            String key_type_total_rooms = (String) stat.keySet().toArray()[0];
+            Vector<Integer> available_rooms = stat.get(key_type_total_rooms);
+            all_available_rooms.addAll(available_rooms);
+            System.out.println("|--- " + type.toString() + " Rooms: " + available_rooms.size() + " out of " + key_type_total_rooms + " vacant");
+            System.out.println("|--- Available Rooms: " + available_rooms);
+        }
+        while (true) {
+            try {
+                picked_room_number = sc_.nextInt();
+                boolean valid_room = all_available_rooms.contains(picked_room_number);
+                if (valid_room) {
+                    sc_.nextLine();
+                    break;
+                }
+            } catch (Exception e) {
+                sc_.nextLine();
+                System.out.println("Unavailable, please try again.");
+            }
+        }
+        return picked_room_number;
+    }
+
+    private int GetIntFromInput() {
+        int value = 0;
+        while (true) {
+            try {
+                value = sc_.nextInt();
+                break;
+            } catch (Exception e) {
+                sc_.nextLine();
+                System.out.println("Unavailable, please try again.");
+            }
+        }
+        return value;
+    }
+
+    private int GetNonZeroIntFromInput() {
+        int value = 0;
+        while (true) {
+            try {
+                value = sc_.nextInt();
+                if (value != 0) {
+                    sc_.nextLine();
+                    break;
+                }
+            } catch (Exception e) {
+                sc_.nextLine();
+                System.out.println("Unavailable, please try again.");
+            }
+        }
+        return value;
+    }
+
+    private float GetFloatFromInput() {
+        float value = 0.0f;
+        while (true) {
+            try {
+                value = sc_.nextFloat();
+                break;
+            } catch (Exception e) {
+                sc_.nextLine();
+                System.out.println("Unavailable, please try again.");
+            }
+        }
+        return value;
+    }
+
+    private float GetNonZeroFloatFromInput() {
+        float value = 0.0f;
+        while (true) {
+            try {
+                value = sc_.nextFloat();
+                if (value != 0.0f) {
+                    sc_.nextLine();
+                    break;
+                }
+            } catch (Exception e) {
+                sc_.nextLine();
+                System.out.println("Unavailable, please try again.");
+            }
+        }
+        return value;
     }
 
     /**
